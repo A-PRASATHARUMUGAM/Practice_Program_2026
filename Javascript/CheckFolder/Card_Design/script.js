ac52e6b461d8c2983694f78e13fe2e2651899c03
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2026-05-29 17:46:58 +0530

    add the practice in the operation.js file

--- Javascript/CheckFolder/Card_Design/script.js
+++ Javascript/CheckFolder/Card_Design/script.js
@@ -29,66 +29,6 @@ console.log("Your Bill Amount is ₹",TotalAmount);
 
 //Your using the constant value you will be put const;
 
-// Operators 
-
-/*
-BDMAS Roles to Working this.
-
-1.Arithmetic Operators:  + -  * ** / % ++  -- 
-2.Assignment Operators: = += -= *= /= %= **=
-3.Comparison Operators: ==  ===  !=  !==   >  <   >=  <= ?:
-4.Logical Operators: && || !
-5.Bitwise Operators: &  |  ~  ^  <<  >>  >>>
-
-   
-*/
-
-
-// Comparison Operators : == 
-let Value1= 5;
-let Value2= "5";
-
-console.log(Value1==Value2);  //True 
-
-// Compariosn Operators: ===
-console.log(Value1===Value2);
-
-//Not Equal and Not Equal 
-console.log("Not Equal ",5!=5); //true 
-
-console.log("Not Equal with D",5!=="6");  //false  
-
-//Square 
-console.log(2*2); //It is normal Multiplication 
-console.log(2**3);//It is Square Root
-
-//Modules
-console.log(10%8);
-
-//Divided
-console.log(10/5);
-
-
-//Assignment 
-
-let Aa=5 
-let Bb=5 
-
-console.log(Aa = Aa +1 );
-//Short and Assignement 
-console.log(Bb += 1 ); 
- 
-
-// Pre/Post Increment and Pre/Post Decrements
-
-
-//Greater Than and Lesser Than 
-
-
-console.log("Greater than :", 10 > 20);//False 
-console.log("Lesser Than :",10<40); //True  
-
-
 
 
 
