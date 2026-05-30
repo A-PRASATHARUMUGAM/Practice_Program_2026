7a9840dfc2ae9b1923c61e4f8985a6248251d7e3
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2026-05-30 12:23:16 +0530

    Create new code using prompt

--- Javascript/3. DataTypes/datatypes.js
+++ Javascript/3. DataTypes/datatypes.js
@@ -1,3 +1,11 @@
 console.log("DataTypes in JavaScript");
 
 
+// Prompt 
+let tickets= prompt("How many tickets do you want?");
+
+let result=Number(tickets*20);
+
+console.log(`You got ${result} tickets`);
+
+console.log(typeof(result));
