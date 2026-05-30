efe5a9cd2240480ba3c9e4558751d73bec7d39d9
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2026-05-30 14:37:14 +0530

    complete the Project  celsius to fahrenheit converter

---
+++ Javascript/Projects/2.Celsius_Fahrenheit_Converter/script.js
@@ -0,0 +1,15 @@
+
+
+
+
+function convert(){
+
+let celsValue=document.getElementById('input').value;
+
+let fahValue=Number((celsValue*9/5) + 32);
+let result=document.getElementById('result');
+result.innerHTML=fahValue+" F";
+
+
+
+}
