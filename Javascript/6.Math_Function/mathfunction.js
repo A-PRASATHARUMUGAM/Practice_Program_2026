9b69af5a29624602bb38df05689f40f8f094c497
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2026-05-30 15:04:41 +0530

    Complete the Math function

--- Javascript/6.Math_Function/mathfunction.js
+++ Javascript/6.Math_Function/mathfunction.js
@@ -1,2 +1,46 @@
-console.log("hello World");
- 
+console.log("Math Function");
+
+//Round 5.3 = 5 / Round 5.8 = 6
+console.log(Math.round(5.4));
+
+//Floor 5.3=  5 / Floor 5.8= 5 
+console.log(Math.floor(5.2));
+
+//Next Number 
+console.log(Math.ceil(5.2));  
+
+//Square root 2^2
+console.log(Math.sqrt(2));
+
+// abs -5 = 5 / abs 5 = 5 
+console.log(Math.abs(5));
+
+// Base and Power 2 ^ 3
+console.log(Math.pow(2,3));
+
+//Minimum Value 
+console.log(Math.min(0,1,2,3,4,5,6));
+
+//Maximum Value  
+console.log(Math.max(0,1,2,3,4,5,6));
+
+//Random Values     
+console.log(Math.floor(Math.random()*6)+1);        
+
+//Math PI it is Constant Value 
+console.log(Math.PI); 
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
