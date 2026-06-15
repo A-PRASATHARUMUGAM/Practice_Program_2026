7f2041c2439aa9d20bf298cfb60d57f032ba993e
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2026-06-15 17:01:23 +0530

    Complete the module in Node.js

---
+++ Node/Common/data.js
@@ -0,0 +1,17 @@
+
+names = ['Prasath','Kandhan','Vignesh','Sabari'];
+let ages = ['24','23','25','26'];
+
+//1
+// console.log(names);
+
+
+//2
+// module.exports = names
+
+
+//3
+module.exports = {
+  names,
+  ages
+}
---
+++ Node/Modules.js
@@ -0,0 +1,13 @@
+const data = require('./Common/data');
+const  os = require('os')
+
+//1
+// console.log(data)
+
+//2
+// console.log(data.allnames)
+// console.log(data.ages)
+
+
+
+console.log(os.homedir());
