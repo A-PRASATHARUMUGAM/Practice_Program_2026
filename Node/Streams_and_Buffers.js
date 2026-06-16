4ecc95b6ce179c6ed3f6a960cdf86c8992012651
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2026-06-16 16:59:18 +0530

    Completed the Streams and Buffers in Node.js

--- Node/Streams_and_Buffers.js
+++ Node/Streams_and_Buffers.js
@@ -1,1 +1,31 @@
-                                                // Streams And Buffer 
+                                                // Streams And Buffer
+
+// 1.createReadStream 
+// 2.writesStream 
+//
+
+const fs =require('fs');
+
+
+// 1.createReadStream 
+const readStream = fs.createReadStream("./docs/HugeFile.txt",{encoding:'utf-8'});
+
+// readStream.on('data',(buffer)=>{
+//     console.log('\nNew Buffer\n')
+//     console.log(buffer);
+// })
+
+
+// 2.writesStream 
+const writesStream= fs.createWriteStream("./docs/copyHugeFile.txt");
+
+// readStream.on('data',(buffer)=>{
+//    writesStream.write('\nNew Buffer\n');
+//    writesStream.write(buffer); 
+// })
+
+
+//Read + write 
+readStream.pipe(writesStream);
+
+
