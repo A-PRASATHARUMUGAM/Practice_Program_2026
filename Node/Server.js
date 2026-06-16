4b7fc449f7d474f93baa982984d584f3e1db5e53
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2026-06-16 18:26:11 +0530

    Refactor server code to improve request logging and enhance code readability

--- Node/Server.js
+++ Node/Server.js
@@ -1,9 +1,25 @@
+/*
+1.req.url - It is return the current URL 
+2.req.method - It is return the which type of Method 
+
+*/
+
+
+// Request 
 const http = require('http');
 
 const server=http.createServer((req,res)=>{
-
     console.log("Request Made");
+    console.log(req.url);
+    console.log(req.method);
+    
+    // res.setHeader('Content-Type','text/plain');
+    // res.write("Hello world ");
+    res.setHeader('Content-Type','text/html');
+    res.write("<h1>Hello world </h1>");
+    res.end()
 })
+
 
 server.listen(3000,'localhost',()=>{
 
