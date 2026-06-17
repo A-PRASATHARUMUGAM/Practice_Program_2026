ea79dd1a7958515c82c36c8692a68c92d3f62e06
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2026-06-17 12:28:23 +0530

    create the new files to practice the HTML file to send the response

--- Node/Server.js
+++ Node/Server.js
@@ -1,23 +1,45 @@
 /*
 1.req.url - It is return the current URL 
 2.req.method - It is return the which type of Method 
-
+3.res.end - It is mention the response is end 
+4.
 */
 
 
 // Request 
 const http = require('http');
+const fs=require('fs')
 
 const server=http.createServer((req,res)=>{
-    console.log("Request Made");
-    console.log(req.url);
-    console.log(req.method);
-    
+
+      
+    // console.log("Request Made");
+    // console.log(req.url);
+    // console.log(req.method);
+
+    // -- Sending the Response 
     // res.setHeader('Content-Type','text/plain');
     // res.write("Hello world ");
-    res.setHeader('Content-Type','text/html');
-    res.write("<h1>Hello world </h1>");
-    res.end()
+    // res.setHeader('Content-Type','text/html');
+    // res.write("<h1>Hello world </h1>");
+    //   res.end(data)
+
+
+
+    //-- Reading the HTML file and Retrun Response HTML file
+    res.setHeader('Content-Type','text/plain');
+    fs.readFile("./docs/index.html",(err,data)=>{
+
+        if(err){
+            console.log(err.message);
+            res.end
+        }else{
+            res.end(data)
+        }
+        
+ 
+    })
+
 })
 
 
