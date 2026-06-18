493220eae5219fd413311dafd8ae5a08fb0bf927
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2026-06-18 12:22:47 +0530

    Completed the Middleware in the Node.js

--- Node/App.js
+++ Node/App.js
@@ -1,6 +1,30 @@
+/*
+1. get - It is get method  
+2. use - It is using for 404 Response you can't found any route at time using to retun 
+3. Middlware -  Browser -> Request -> Server(Middleware) -> Response -> Browser
+4. 
+
+*/ 
 const exp = require('express')
+const morgan = require('morgan')
 const app= exp() 
 app.listen(3000)
+//Morgan
+app.use(morgan('dev'));
+
+//Middleware 
+app.use((req,res,next)=>{
+    console.log('Request Recieved');
+    console.log(req.host);
+    console.log(req.path);
+    console.log(req.method);
+    next();
+});
+
+app.use((req,res,next)=>{
+    console.log("Middleware 2");
+    next()
+});
 
 
 app.get('/',(req,res)=>{
@@ -20,8 +44,11 @@ app.get("/join",(req,res)=>{
 });
 
 app.use((req,res)=>{
-    res.status(404).sendFile('./docs/index.html',{root:__dirname});
+    res.status(404).sendFile('./docs/NotFound.html',{root:__dirname});
 })
+
+
+
 
 // { root: 'D:\\Program\\Practice_Program_2026\\Node' }
 // console.log({root:__dirname})
