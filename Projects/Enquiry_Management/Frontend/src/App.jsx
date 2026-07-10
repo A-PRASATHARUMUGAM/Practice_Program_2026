776ba26bad06d5e4ba0cfc724c4d14a738c2e88d
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2026-07-10 12:51:54 +0530

    Created the All Project Setup and add some project requirments

--- Projects/Enquiry_Management/Frontend/src/App.jsx
+++ Projects/Enquiry_Management/Frontend/src/App.jsx
@@ -1,22 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
 
 function App() {
   return (
     <>
-    <div className="min-h-screen flex items-center justify-center bg-green-500">
-      <h1 className="text-5xl font-bold text-white">
 
-            Completed the Frontend Setup 
+    {/* Main Container */}
+
+    <div className="">
+
+
+      <h1 className="text-white bg-green-400 font-medium">
+
+             The Frontend Setup is Completed
 
       </h1>
 
+
+          
     </div>
     </>
   )
-}
+} 
 
 export default App
